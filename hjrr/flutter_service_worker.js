b7be346d14e8f4aaeccb8776a8fc004d8d3c5dae
Brockaaa <brocka.dev@gmail.com>
2024-10-24 20:29:14 +0200

    3

--- hjrr/flutter_service_worker.js
+++ hjrr/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "55cd4a4b645dbdcf49c9dd6ccd48fe6a",
+const RESOURCES = {"flutter_bootstrap.js": "944fb142504220ca7e8d8bb75e432187",
 "version.json": "129ef0fb5c7b8201611c316f80cf8d49",
-"index.html": "daa63e11531d0cf87919adbeb1e0da1d",
-"/": "daa63e11531d0cf87919adbeb1e0da1d",
-"main.dart.js": "72c60abd6eb92df8e874492aef00e5ff",
+"index.html": "c54fd9b8c217ae168cd02f91c118d429",
+"/": "c54fd9b8c217ae168cd02f91c118d429",
+"main.dart.js": "7ccb9333a7d0050eed4c0d81ccb82394",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "b752b2040427d9e16e8fa09ee96d54fe",
 "icons/Icon-192.png": "f63876a97d1f89900e3d20ec82023f35",
