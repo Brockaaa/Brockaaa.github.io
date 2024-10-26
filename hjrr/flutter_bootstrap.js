5d9c7a5ed6be0b85304aace39b35a31e423a2464
Brockaaa <brocka.dev@gmail.com>
2024-10-26 03:08:00 +0200

    d

--- hjrr/flutter_bootstrap.js
+++ hjrr/flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"edd8546116457bdf1c5bdfb13ecb9463d2bb5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3396955017"
+    serviceWorkerVersion: "2912436545"
   }
 });
